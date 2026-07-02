89e767026e80d963fabe47b8030143b68680862f
Mason363 <masonchen204@gmail.com>
2026-07-01 22:37:11 -0400

    Initial Commit

---
+++ Tactile/TactileApp.swift
@@ -0,0 +1,17 @@
+//
+//  TactileApp.swift
+//  Tactile
+//
+//  Created by Mason Chen on 7/1/26.
+//
+
+import SwiftUI
+
+@main
+struct TactileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
